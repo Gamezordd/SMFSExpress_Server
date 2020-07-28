fa3dd82ea5eee592a05a22ddc27de5f60e6a025c
Amartya Mishra <kushmishra8@gmail.com>
2020-07-29 03:23:19 +0530

    config set to mongo

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-/*exports.config = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://frontend:amartya1@ds339648.mlab.com:39648/heroku_fb5q4zq7'
-}*/
 exports.config = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/SM-FS'
+    'mongoUrl': 'mongodb://frontend:amartya1@ds339648.mlab.com:39648/heroku_fb5q4zq7'
 }
+/*exports.config = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/SM-FS'
+}*/
